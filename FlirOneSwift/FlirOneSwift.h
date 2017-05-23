80396f30e1dca751e00c5abbe9addbffe958b1c8
Andrew Heim <jruhym@gmail.com>
2017-05-23 18:40:18 -0400

    add basic project into folder

---
+++ FlirOneSwift/FlirOneSwift.h
@@ -0,0 +1,19 @@
+//
+//  FlirOneSwift.h
+//  FlirOneSwift
+//
+//  Created by Andrew Heim on 5/23/17.
+//  Copyright © 2017 UltravioletCatastrophe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlirOneSwift.
+FOUNDATION_EXPORT double FlirOneSwiftVersionNumber;
+
+//! Project version string for FlirOneSwift.
+FOUNDATION_EXPORT const unsigned char FlirOneSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlirOneSwift/PublicHeader.h>
+
+
